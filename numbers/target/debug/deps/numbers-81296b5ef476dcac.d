a25879aa48efc1e043e529620dcc25521de291ee
CelestialMelody <celetialmelodygo@gmail.com>
2022-07-01 10:24:31 +0800

    day two

---
+++ numbers/target/debug/deps/numbers-81296b5ef476dcac.d
@@ -0,0 +1,5 @@
+/home/melody/Code/rust/rust-begin/numbers/target/debug/deps/numbers-81296b5ef476dcac.rmeta: src/main.rs
+
+/home/melody/Code/rust/rust-begin/numbers/target/debug/deps/numbers-81296b5ef476dcac.d: src/main.rs
+
+src/main.rs:
